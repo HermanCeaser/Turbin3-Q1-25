8e6ad3f99f10eeeab5b268c6758f9b9c53087982
Herman Ceaser <ceaserbanks09@gmail.com>
2025-01-21 10:53:49 +0300

    deposited 1 SOL to the vault address: `G9KqPVeTuXEDagv61f4mQ3ZFaB2nsQbPNmWM9dTxnsHU`

--- solana-starter/ts/cluster1/vault_deposit.ts
+++ solana-starter/ts/cluster1/vault_deposit.ts
@@ -4,6 +4,7 @@ import {
   SystemProgram,
   PublicKey,
   Commitment,
+  LAMPORTS_PER_SOL,
 } from "@solana/web3.js";
 import {
   Program,
@@ -13,7 +14,7 @@ import {
   BN,
 } from "@coral-xyz/anchor";
 import { WbaVault, IDL } from "./programs/wba_vault";
-import wallet from "./wallet/wba-wallet.json";
+import wallet from "../wba-wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -30,28 +31,45 @@ const provider = new AnchorProvider(connection, new Wallet(keypair), {
 });
 
 // Create our program
-const program = new Program<WbaVault>(IDL, "<address>" as Address, provider);
+const program = new Program<WbaVault>(
+  IDL,
+  "D51uEDHLbWAxNfodfQDv7qkp8WZtxrhi3uganGbNos7o" as Address,
+  provider
+);
 
 // Create a random keypair
-const vaultState = new PublicKey("<address>");
+const vaultState = new PublicKey(
+  "3mMAKP936LTcb2Li2gMzxW1VjDFkqNMEK3Cq52D7smUZ"
+);
 // Create the PDA for our enrollment account
-// const vaultAuth = ???
+const [vaultAuth, _bump] = PublicKey.findProgramAddressSync(
+  [Buffer.from("auth"), vaultState.toBuffer()],
+  program.programId
+);
 
-// Create the vault key
-// const vault = ???
+// Create the vault key G9KqPVeTuXEDagv61f4mQ3ZFaB2nsQbPNmWM9dTxnsHU
+const [vault, _bump2] = PublicKey.findProgramAddressSync(
+  [Buffer.from("vault"), vaultAuth.toBuffer()],
+  program.programId
+);
 
 // Execute our enrollment transaction
 (async () => {
   try {
-    // const signature = await program.methods
-    // .deposit(new BN(<number>)    )
-    // .accounts({
-    //    ???
-    // })
-    // .signers([
-    //     keypair
-    // ]).rpc();
-    // console.log(`Deposit success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    const signature = await program.methods
+      .deposit(new BN(1 * LAMPORTS_PER_SOL))
+      .accounts({
+        vaultState,
+        owner: keypair.publicKey,
+        vault: vault,
+        vaultAuth: vaultAuth,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([keypair])
+      .rpc();
+    console.log(
+      `Deposit success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`
+    );
   } catch (e) {
     console.error(`Oops, something went wrong: ${e}`);
   }
