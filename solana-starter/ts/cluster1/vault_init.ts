0b36d10547c71dc25662cb40e6c4ca5aee45171d
Herman Ceaser <ceaserbanks09@gmail.com>
2025-01-21 10:21:39 +0300

    initialized vault

--- solana-starter/ts/cluster1/vault_init.ts
+++ solana-starter/ts/cluster1/vault_init.ts
@@ -7,7 +7,7 @@ import {
 } from "@solana/web3.js";
 import { Program, Wallet, AnchorProvider, Address } from "@coral-xyz/anchor";
 import { WbaVault, IDL } from "./programs/wba_vault";
-import wallet from "./wallet/wba-wallet.json";
+import wallet from "../wba-wallet.json";
 /// J8qKEmQpadFeBuXAVseH8GNrvsyBhMT8MHSVD3enRgJz
 
 // Import our keypair from the wallet file
@@ -33,24 +33,34 @@ const program = new Program<WbaVault>(
 
 // Create a random keypair
 const vaultState = Keypair.generate();
-console.log(`Vault public key: ${vaultState.publicKey.toBase58()}`);
+console.log(`Vault public key: ${vaultState.publicKey.toBase58()}`); //3mMAKP936LTcb2Li2gMzxW1VjDFkqNMEK3Cq52D7smUZ
 
 // Create the PDA for our enrollment account
 // Seeds are "auth", vaultState
-// const vaultAuth = ???
+const vaultAuth = PublicKey.findProgramAddressSync(
+  [Buffer.from("auth"), vaultState.publicKey.toBuffer()],
+  program.programId
+);
 
 // Create the vault key
 // Seeds are "vault", vaultAuth
-// const vault = ???
+const vault = PublicKey.findProgramAddressSync(
+  [Buffer.from("vault"), vaultAuth[0].toBuffer()],
+  program.programId
+);
 
 // Execute our enrollment transaction
 (async () => {
   try {
-    // const signature = await program.methods.initialize()
-    // .accounts({
-    //     ???
-    // }).signers([keypair, vaultState]).rpc();
-    // console.log(`Init success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    const signature = await program.methods.initialize()
+    .accounts({
+      vaultState: vaultState.publicKey,
+      vault: vault[0],
+      vaultAuth: vaultAuth[0],
+      systemProgram: SystemProgram.programId,
+      owner: keypair.publicKey
+    }).signers([keypair, vaultState]).rpc();
+    console.log(`Init success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
   } catch (e) {
     console.error(`Oops, something went wrong: ${e}`);
   }
