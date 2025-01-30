c7b129ea2d8011cca9f005b455b5a078d5bbf01d
Herman Ceaser <ceaserbanks09@gmail.com>
2025-01-30 16:37:48 +0300

    added marketplace solana program: - Has the initialize instruction to create amarketplace - Has a listing Instruction to list an NFT on Marketplace - Has a purchase_nft instruction to buy an NFT - Has a delist instruction to rescind the NFT from the marketplace

---
+++ marketplace/programs/marketplace/src/lib.rs
@@ -0,0 +1,43 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("6zdR8AgbvNJofyWxFqUGe5qVpvrUdFSNmJNz5Ln7Syek");
+
+#[program]
+pub mod marketplace {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.init(name, fee, &ctx.bumps)?;
+        Ok(())
+    }
+
+    pub fn list_nft(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()?;
+
+        Ok(())
+    }
+
+    pub fn purchase(ctx: Context<Purchase>) -> Result<()> {
+        ctx.accounts.send_sol()?;
+        ctx.accounts.send_nft()?;
+        ctx.accounts.close_mint_vault()?;
+
+        Ok(())
+    }
+
+    pub fn delist_nft(ctx: Context<Delist>) -> Result<()> {
+        ctx.accounts.withdraw_nft()?;
+        Ok(())
+    }
+
+}
