7d0925179350e1e3cc840562601ce3637d1130f3
Herman Ceaser <ceaserbanks09@gmail.com>
2025-02-20 23:20:56 +0300

    added nft staking program

---
+++ nft-staking/programs/nft-staking/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("5gThiS1WgXcpeTrKWDEr7gca93oVwAX1S5eyUVgQRPyP");
+
+#[program]
+pub mod nft_staking {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
